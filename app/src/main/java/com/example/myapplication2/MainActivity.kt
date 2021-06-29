a62f65ef465ea725e05e21d9c2b4de8608734190
unknown <siddharth9k9@gmail.com>
2021-06-29 18:57:43 +0530

    asd

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //assdas
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+}
